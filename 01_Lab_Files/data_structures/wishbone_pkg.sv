dd8a0b6864dbc6efa4713d25e1eaaec6f18ff18c
Jordan Deitsch <jordan.deitsch@olympus.com>
2025-08-29 17:05:55 -0600

    Initial edits to first lab

--- 01_Lab_Files/data_structures/wishbone_pkg.sv
+++ 01_Lab_Files/data_structures/wishbone_pkg.sv
@@ -1,11 +1,23 @@
-
-
 // LAB - create a package named wishbone_pkg
+package wishbone_pkg
+	
+	// LAB - declare an enumeration of wishbone operation types
+	typedef enum {
+		NONE,
+		WRITE,
+		READ,
+		RMW,
+		WAIT_IRQ
+	} wb_txn_t;
+	
+	// LAB - declare a wishbone bus struct
+	typedef struct {
+		wb_txn_t txn_type;
+		bit[31:0] adr;
+		logic[31:0] data;
+		int unsigned count;
+		bit[7:0] byte_sel;
+	} wb_txn;
+	
+endpackage
 
-
-  // LAB - declare an enumeration of wishbone operation types
-
-
-
-
-  // LAB - declare a wishbone bus struct
